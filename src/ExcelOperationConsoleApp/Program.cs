1f2fd6cb03878d917fc641fa489fb5972b096856
きなこもち <kinakomotitti0210@yahoo.co.jp>
2021-07-11 19:10:40 +0900

    修正

--- src/ExcelOperationConsoleApp/Program.cs
+++ src/ExcelOperationConsoleApp/Program.cs
@@ -7,6 +7,7 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Text.RegularExpressions;
 
     #endregion
 
@@ -23,10 +24,17 @@
 
         class PopulationByAge
         {
-            public int Age { get; set; }
-            public int NumberOfMale { get; set; }
-            public int NumberOfFemale { get; set; }
+            public string Age { get; set; }
+            public int Male { get; set; }
+            public int Female { get; set; }
         }
+
+        class HeaderItem
+        {
+            public IXLAddress Address { get; set; }
+            public string TypeOfValue { get; set; }
+        }
+
 
         void Run(string filePath)
         {
@@ -34,54 +42,69 @@
             {
                 IXLWorksheet worksheet = null;
                 workbook.Worksheets.TryGetWorksheet("第１表", out worksheet);
-                var result = this.GetAllDataFromWorksheet(worksheet);
+
+                var headers = new Dictionary<string, HeaderItem>()
+                {
+                    { "Age",new HeaderItem(){TypeOfValue =nameof(String) }},
+                    { "Male", new HeaderItem() { TypeOfValue = nameof(Int32) }},
+                    { "Female", new HeaderItem() { TypeOfValue = nameof(Int32) }}
+                };
+                var result = this.GetAllDataFromWorksheet(worksheet, headers, "Age");
                 Console.WriteLine("Age,Male,Female");
                 foreach (var item in result)
                 {
-                    Console.WriteLine($"{item.Age},{item.NumberOfMale},{item.NumberOfFemale}");
+                    Console.WriteLine($"{item.Age},{item.Male},{item.Female}");
                 }
             }
         }
 
-        List<PopulationByAge> GetAllDataFromWorksheet(IXLWorksheet worksheet)
+        List<PopulationByAge> GetAllDataFromWorksheet(IXLWorksheet worksheet, Dictionary<string, HeaderItem> headers, string primaryKey)
         {
             var result = new List<PopulationByAge>();
-            //Headerの位置は定義する必要がある・・・
-            var headerCells = worksheet.Range("A1:T19").Cells();
+            var headerCells = worksheet.Cells();
 
-            //Headerの列を特定する。
-            var ageColumn = GetSpecificHeaderColumn(headerCells, "Age");
-            var maleColumn = GetSpecificHeaderColumn(headerCells, "Male");
-            var femaleColumn = GetSpecificHeaderColumn(headerCells, "Female");
 
-            //表のBody部の設定をする。
-            var lastRowNumber = worksheet.LastRowUsed().Cell(ageColumn.ColumnNumber).Address.RowNumber;
-            var lastColNumber = worksheet.LastColumnUsed().Cell(ageColumn.RowNumber).Address.ColumnNumber;
+            foreach (var header in headers)
+            {
+                header.Value.Address = GetSpecificHeaderColumn(headerCells, header.Key);
+            }
+
+            var lastRowNumber = worksheet.LastRowUsed().Cell(headers[primaryKey].Address.ColumnNumber).Address.RowNumber;
+            var lastColNumber = worksheet.LastColumnUsed().Cell(headers[primaryKey].Address.RowNumber).Address.ColumnNumber;
             var bodyCells = worksheet.Range($"A1:{ToColumnName(lastColNumber)}{lastRowNumber}").Cells();
 
-            //Headerの列毎に、情報を取得する。
-            var ageColumns = bodyCells.Where(i => i.Address.ColumnNumber == ageColumn.ColumnNumber).Select(i => i);
-            var maleColumns = bodyCells.Where(i => i.Address.ColumnNumber == maleColumn.ColumnNumber).Select(i => i);
-            var femaleColumns = bodyCells.Where(i => i.Address.ColumnNumber == femaleColumn.ColumnNumber).Select(i => i);
+            var primaryRows = bodyCells.Where(i => i.Address.ColumnNumber == headers[primaryKey].Address.ColumnNumber).Select(i => i);
 
-            //必要な情報だけをPOCOに設定していく。
-            //主キーは、年齢列とする。
-            foreach (var age in ageColumns)
+            foreach (var primaryRow in primaryRows)
             {
-                System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex("^[0-9]");
-                if (regex.IsMatch(age.Value.ToString().Trim()) == false) continue;
+                Regex regex = new Regex("^[0-9]");
+                if (regex.IsMatch(primaryRow.Value.ToString().Trim()) == false) continue;
 
-                var excelRow = age.Address.RowNumber;
-                result.Add(new PopulationByAge()
+                var excelRow = primaryRow.Address.RowNumber;
+                var newItem = new PopulationByAge();
+                foreach (var header in headers)
                 {
-                    Age = int.Parse(age.Value.ToString().Trim().Split(" ").First()),
-                    NumberOfFemale = maleColumns.Where(i => i.Address.RowNumber == excelRow)
-                                         .Select(i => int.Parse(i.Value.ToString().Trim().Replace(",", string.Empty)))
-                                         .FirstOrDefault(),
-                    NumberOfMale = femaleColumns.Where(i => i.Address.RowNumber == excelRow)
-                                              .Select(i => int.Parse(i.Value.ToString().Trim().Replace(",", string.Empty)))
-                                              .FirstOrDefault()
-                });
+                    var valueObject = worksheet.Cells().Where(i => i.Address.RowNumber == excelRow &&
+                                                             i.Address.ColumnNumber == headers[header.Key].Address.ColumnNumber)
+                                         .Select(i => i.Value)
+                                         .FirstOrDefault();
+
+                    switch (header.Value.TypeOfValue)
+                    {
+                        case nameof(String):
+                            var valueString = valueObject.ToString().Trim().Split(" ").First();
+                            typeof(PopulationByAge).GetProperty(header.Key).SetValue(newItem, valueString);
+                            break;
+                        case nameof(Int32):
+                            var valueInt = int.Parse(valueObject.ToString().Trim());
+                            typeof(PopulationByAge).GetProperty(header.Key).SetValue(newItem, valueInt);
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+                result.Add(newItem);
             }
             return result;
         }
@@ -92,10 +115,12 @@
                         .Select(i => i.Address).FirstOrDefault();
         }
 
+        //https://qiita.com/omochi_motimoti/items/b6261e3cacda11d460dd#%E6%95%B0%E5%80%A4--%E3%82%AB%E3%83%A9%E3%83%A0%E6%96%87%E5%AD%97 を参照。
         string ToColumnName(int source)
         {
+            var numberOfAlphabet = 26;
             if (source < 1) return string.Empty;
-            return ToColumnName((source - 1) / 26) + (char)('A' + ((source - 1) % 26));
+            return ToColumnName((source - 1) / numberOfAlphabet) + (char)('A' + ((source - 1) % numberOfAlphabet));
         }
     }
 }
