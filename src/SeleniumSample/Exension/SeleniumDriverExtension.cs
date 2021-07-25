84f36960fab843ec96e170eff0870407e919264e
きなこもち <kinakomotitti0210@yahoo.co.jp>
2021-07-25 19:17:25 +0900

    modify

--- src/SeleniumSample/Exension/SeleniumDriverExtension.cs
+++ src/SeleniumSample/Exension/SeleniumDriverExtension.cs
@@ -9,9 +9,18 @@ namespace SeleniumSample.Exension
 {
     public static class SeleniumDriverExtension
     {
+        //TODO :https://www.w3.org/TR/webdriver/#keyboard-actions
         public static void SendTabKey(this IWebElement element)
         {
             element.SendKeys("\uE004");
+        }
+
+        public static void SendTabKey(this IWebElement element, int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                element.SendTabKey();
+            }
         }
         public static void DelayAction(this IWebElement element, int millisecondsDelay)
         {
